45a47eaa9d1525f7e5f6887141507aaa18872a6a
mcreddi1 <morachinnapareddy1996@gmail.com>
2024-09-22 20:32:26 +0530

    tf workspaces

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "terraform" {
     #count = terraform.workspace == "prod" ? 2 : 1
     ami = "ami-09c813fb71547fc4f"
     instance_type = lookup(var.instance_type, terraform.workspace)
-    vpc_security_group_ids = ["sg-09a300a3729062c5a"]
+    vpc_security_group_ids = ["sg-0b764df7602aee442"]
     tags = {
         Name = "terraform-${terraform.workspace}"
     }
--- provider.tf
+++ provider.tf
@@ -7,10 +7,10 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "81s-remote-state"
+    bucket = "chinnadev"
     key    = "workspaces"
     region = "us-east-1"
-    dynamodb_table = "81s-locking"
+    dynamodb_table = "dynamodev"
   }
 }
 
